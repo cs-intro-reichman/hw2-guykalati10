417b8cf1e0286ad6040113510b8552c7a69be9d6
guy kalati <guykalati10@gmail.com>
2024-11-18 22:58:18 +0200

    forth update

--- Cheers.java
+++ Cheers.java
@@ -9,7 +9,7 @@ public class Cheers {
 
                 for(int i =0 ; i<upperS.length() ; i++){
 
-                        if(( upperS.charAt(i) == 'A' )||( upperS.charAt(i) == 'I' )||( upperS.charAt(i) == 'F' )||( upperS.charAt(i) == 'H' )||( upperS.charAt(i) == 'I' )||( upperS.charAt(i) == 'L' )||
+                        if(( upperS.charAt(i) == 'A' )||( upperS.charAt(i) == 'E' )||( upperS.charAt(i) == 'F' )||( upperS.charAt(i) == 'H' )||( upperS.charAt(i) == 'I' )||( upperS.charAt(i) == 'L' )||
                         ( upperS.charAt(i) == 'M' )||( upperS.charAt(i) == 'N' )||( upperS.charAt(i) == 'O' )||( upperS.charAt(i) == 'R' )||( upperS.charAt(i) == 'S' )||( upperS.charAt(i) == 'X' )){
                         System.out.println("Give me an " + upperS.charAt(i) + ": " + upperS.charAt(i) + "!");
                         }
