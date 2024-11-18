e3c1e4fadfedb444289eebe97b00c8a6480eda5b
guy kalati <guykalati10@gmail.com>
2024-11-18 22:45:15 +0200

    third update

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,35 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+
+                String s = args[0];
+                int num = Integer.parseInt(args[1]);
+
+                String upperS = s.toUpperCase();
+
+                for(int i =0 ; i<upperS.length() ; i++){
+
+                        if(( upperS.charAt(i) == 'A' )||( upperS.charAt(i) == 'I' )||( upperS.charAt(i) == 'F' )||( upperS.charAt(i) == 'H' )||( upperS.charAt(i) == 'I' )||( upperS.charAt(i) == 'L' )||
+                        ( upperS.charAt(i) == 'M' )||( upperS.charAt(i) == 'N' )||( upperS.charAt(i) == 'O' )||( upperS.charAt(i) == 'R' )||( upperS.charAt(i) == 'S' )||( upperS.charAt(i) == 'X' )){
+                        System.out.println("Give me an " + upperS.charAt(i) + ": " + upperS.charAt(i) + "!");
+                        }
+                        else{
+                                System.out.println("Give me a " + upperS.charAt(i) + ": " + upperS.charAt(i) + "!");
+                        }
+
+                }
+
+                System.out.println("What does that spell?");
+
+                for(int i = 0 ; i < num ; i++){
+                        System.out.println(upperS + "!!!");
+                }
+
+
+        
+
+
+
+	    
         }
 }
